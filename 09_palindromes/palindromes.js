3156806afc05b5d040af65540f1c6a4f8fbf8bd1
leelethal816 <zhangtianyi816@gmail.com>
2024-10-31 23:05:37 -0500

    Finish exercise 8 & 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    arr = str.replaceAll(",", "").replaceAll(".", "").replaceAll("!", "").replaceAll(" ", "").toLowerCase().split("");
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== arr[arr.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
