13a0234dbe36b5c82e4726dcf22ee5fa956da71b
leelethal816 <zhangtianyi816@gmail.com>
2024-11-01 20:33:11 -0500

    Finish exercise 10, 11, 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    if (position < 0) {
+        return "OOPS";
+    }
+    position = Number(position);
+    let arr = [0, 1];
+    for (let i = 2; i <= position; i++) {
+        arr.push(arr[i - 1] + arr[i - 2]);
+    }
+    return arr[position];
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    for (let each of people) {
+        if (!('yearOfDeath' in each)) {
+            const today = new Date();
+            each.yearOfDeath = today.getFullYear();
+        }
+    }
+    people.map(person => person.age = person.yearOfDeath - person.yearOfBirth);
+    people.sort((a, b) => a.age > b.age ? -1 : 1)[0];
+    people = people[0];
+    return people;
 };
 
 // Do not edit below this line
